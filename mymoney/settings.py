1bcea91d01dba44711b746f624b2a353c158aab9
Carolina Carranza <cmcc1505@gmail.com>
2021-10-10 23:15:14 -0600

    Add: DB connection base

--- mymoney/settings.py
+++ mymoney/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'mymoney.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg3',
+        'NAME': 'my_money',
+        'USER': 'postgres',
+        'PASSWORD': '070067',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
