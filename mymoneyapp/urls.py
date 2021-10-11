ea8cc7906944106541a076888b1f9df2b2c4cbd2
Carolina Carranza <00008119@uca.edu.sv>
2021-10-11 17:08:30 -0600

    Add: Register, login and logout users

--- mymoneyapp/urls.py
+++ mymoneyapp/urls.py
@@ -4,11 +4,12 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("log-in", views.login, name="log-in"),
+    path("log-in", views.log_in, name="log-in"),
     path("sign-up", views.sign_up, name="sign-up"),
     path("general", views.general, name="general"),
     path("account", views.account, name="account"),
     path("edit-record", views.edit_record, name="edit-record"),
     path("add-account", views.add_account, name="add-account"),
     path("add-record", views.add_record, name="add-record"),
+    path("logout", views.logout_view, name="logout")
 ]
--- mymoneyapp/views.py
+++ mymoneyapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
+from django.db import IntegrityError
 
 from .models import User, Category, AccountType, Account, Record
 
@@ -9,10 +11,56 @@ def index(request):
     return render(request, 'landing.html')
 
 def sign_up(request):
-    return render(request, 'sign-up.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        name = request.POST['name'].split(sep=" ", maxsplit=1)
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm = request.POST['confirm-password']
 
-def login(request):
-    return render(request, 'login.html')
+        if password != confirm:
+            return render(request, 'message.html', {
+                "message": "Passwords do not match"
+            })
+        
+        try:
+            new_user = User.objects.create_user(username, email, password)
+            new_user.first_name = name[0]
+            new_user.last_name = name[1]
+            new_user.save()
+
+        except IntegrityError:
+            return render(request, "message.html", {
+                "message": "Username already taken."
+            })
+        login(request, new_user)
+        return HttpResponseRedirect(reverse("general"))
+    else:
+        return render(request, 'sign-up.html')
+
+def log_in(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+            # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("general"))
+        else:
+            return render(request, "message.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, 'login.html')
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"), {
+})
 
 def general(request):
     return render(request, 'general.html')
