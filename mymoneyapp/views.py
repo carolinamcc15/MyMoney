ff43c95fe05afc1cf1256849b9a073f9f7442200
Carolina Carranza <00008119@uca.edu.sv>
2021-10-11 23:05:52 -0600

    Add: Cards render depnding on ulogged user

--- mymoneyapp/views.py
+++ mymoneyapp/views.py
@@ -1,14 +1,99 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse
+import datetime
 from django.db import IntegrityError
 
 from .models import User, Category, AccountType, Account, Record
 
 # Create your views here.
 def index(request):
+    user = request.user
+
+    if user.is_authenticated:
+        return HttpResponseRedirect(reverse("general"))
+
     return render(request, 'landing.html')
+
+
+@login_required
+def general(request):
+    user = request.user
+    accounts = Account.objects.filter(username = user)
+    records = Record.objects.filter(username = user)
+    total = 0
+    recent_records = []
+    
+    for account in accounts:
+        total = total + account.current_balance
+
+    for i in range(len(records) - 1, len(records) - 5, -1):
+        recent_records.append(records[i])
+
+    return render(request, 'general.html', {
+        "accounts": accounts,
+        "total": total,
+        "recent_records": recent_records
+    })
+
+
+@login_required
+def account(request):
+    return render(request, 'account.html')
+
+
+@login_required
+def edit_record(request):
+    return render(request, 'edit-record.html')
+
+
+@login_required
+def add_account(request):
+    if request.method == "POST":
+        acc_type =  request.POST['account-type']
+
+        new_account = Account(
+            username =request.user, 
+            #Gets the selected category
+            acc_type = AccountType.objects.get(acc_type = acc_type), 
+            name = request.POST['account-name'], 
+            initial_balance = request.POST['balance'], 
+            current_balance = request.POST['balance'])
+
+        new_account .save()
+        return HttpResponseRedirect(reverse("general"))
+
+    return render(request, 'add-account.html', {
+        "types": AccountType.objects.all()
+    })
+
+
+@login_required
+def add_record(request):
+    if request.method == "POST":
+        category =  request.POST['category']
+        account = request.POST['account']
+
+        new_record = Record(
+            username =request.user, 
+            account = Account.objects.get(name = account),
+            category = Category.objects.get(category = category),
+            is_income = request.POST.get("is-income", None),
+            quantity = request.POST['quantity'], 
+            date = datetime.datetime.now().strftime("%Y-%m-%d"),
+            description = request.POST['description']
+            )
+
+        new_record .save()
+        return HttpResponseRedirect(reverse("general"))
+
+    return render(request, 'add-record.html', {
+        "categories": Category.objects.all(),
+        "accounts": Account.objects.all()
+    })
+
 
 def sign_up(request):
     if request.method == 'POST':
@@ -38,6 +123,7 @@ def sign_up(request):
     else:
         return render(request, 'sign-up.html')
 
+
 def log_in(request):
     if request.method == "POST":
 
@@ -57,28 +143,9 @@ def log_in(request):
     else:
         return render(request, 'login.html')
 
+
+@login_required
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse("index"), {
 })
-
-def general(request):
-    return render(request, 'general.html')
-
-def account(request):
-    return render(request, 'account.html')
-
-def edit_record(request):
-    return render(request, 'edit-record.html')
-
-def add_account(request):
-    return render(request, 'add-account.html', {
-        "types": AccountType.objects.all()
-    })
-
-def add_record(request):
-    return render(request, 'add-record.html', {
-        "categories": Category.objects.all(),
-        "accounts": Account.objects.all()
-    })
-
