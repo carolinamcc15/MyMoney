3d9a68ef4a21d618a052a70e413d734ad18f4c2d
Carolina Carranza <00008119@uca.edu.sv>
2021-10-30 05:04:24 +0000

    Add: Server configuration

--- mymoney/settings.py
+++ mymoney/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nh@etjqmbq$rae6uk-2me-nqpb1*0*ux*gcqkf84r$@g5#)jh^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.33']
 
 # Application definition
 
@@ -134,6 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = Path.joinpath(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
