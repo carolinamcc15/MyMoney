fb0ab58b20d3c1befd7af41db0130bd8dc68c9d8
Carolina Carranza <cmcc1505@gmail.com>
2021-11-01 16:58:48 -0600

    Add: Server error view

--- mymoneyapp/views.py
+++ mymoneyapp/views.py
@@ -417,3 +417,6 @@ def logout_view(request):
 
 def error_404(request, exception):
     return render(request, 'not-found.html')
+
+def error_500(request, exception):
+    return render(request, 'server-error.html')
